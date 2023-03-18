4ae529d04d69a79b543402de4ed5a1a63d3bb1d3
axrorbekdf <axrorbektatu@gmail.com>
2023-03-18 15:38:45 +0500

    Article card install

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
-        <h1>Home Page  {{ counter }}</h1>
-        <div class="w-25 d-flex btn-group">
+        <h1 class="d-none">Counter:  {{ counter }}</h1>
+        <div class="w-25 d-flex btn-group d-none">
             <Button @click="inEventHandler"> + </Button>
     
             <Button @click="deEventHandler"> - </Button>
+        </div>
+        
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+            <ArticleCard v-for="article in data" :key="article.id" :article="article"/>
         </div>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import ArticleCard  from '@/components/ArticleCard.vue'
 export default{
+    components:{ArticleCard},
     methods: {
         inEventHandler(){
             this.$store.commit('increment')
@@ -24,7 +30,7 @@ export default{
         ...mapState({
             data: state => state.articles.data,
             isLoading: state => state.articles.isLoading,
-            data: state => state.articles.error,
+            error: state => state.articles.error,
         }),
         counter(){
             return this.$store.state.counter.count
